9f36e87db2f7ef97859dc80455ddc3b8906c3a00
srinivas52 <srinivas.t252@outlook.com>
2016-04-29 15:15:28 +0530

    first commit

--- Scripts/installphpadmin.sh
+++ Scripts/installphpadmin.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-yum install http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
-y
+yum -y  install http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
 yum -y install phpmyadmin > /var/log/installphp.out 2>&1
 
--- Scripts/installphppec.sh
+++ Scripts/installphppec.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+yum -y install php-mysql php-gd php-imap php-ldap php-mbstring php-odbc php-pear php-xml php-xmlrpc
 yum -y install php-pecl-apc > /var/log/installphppec.out 2>&1
 
